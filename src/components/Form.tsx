40b1674c40658608e90947350d19760a19c4f3a4
karl <karl@studio206.dev>
2022-12-02 12:49:44 +0000

    update form tsx

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,17 +1,21 @@
-import React, { FormEvent } from "react";
+import React, { FormEvent, HTMLProps } from "react";
 
-interface FormProps {
+interface FormProps extends HTMLProps<HTMLFormElement> {
   children: React.ReactNode;
   onSubmit: (event: FormEvent<HTMLFormElement>) => void;
 }
 
-const Form = ({ children, onSubmit }: FormProps) => {
+const Form = ({ children, onSubmit, ...props }: FormProps) => {
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     onSubmit(e);
   };
 
-  return <form onSubmit={handleSubmit}>{children}</form>;
+  return (
+    <form onSubmit={handleSubmit} {...props}>
+      {children}
+    </form>
+  );
 };
 
 export default Form;

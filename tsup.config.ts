af2c14a705dc5971b3efc3f9bab12b0bd25d68ae
karl <karl@studio206.dev>
2024-03-14 14:18:44 +0000

    updates

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
+  dts: true,
+  splitting: false,
   sourcemap: true,
   clean: true,
   external: ["react", "react-dom"],
-  dts: true,
 });
